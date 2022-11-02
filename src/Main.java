31938d30de450c6641afd995b3133b369c38b8ce
ShevoFirst <play.job@yandex.ru>
2022-11-02 20:14:54 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 1.
+        //Задание 1
         short month = 0;
         float total = 0;
         while (total < 2_459_000) {
